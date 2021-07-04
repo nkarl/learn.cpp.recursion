d693814d6e8bf3faee612eea3ff714846f1fb599
nkarl <nordenk@pm.me>
2021-07-04 09:34:48 -0700

    updated

--- recurr.cpp
+++ recurr.cpp
@@ -10,47 +10,41 @@ using std::unordered_map;
 typedef long double BigBoi;
 
 // static map container for generic strings
-static unordered_map<string, string> generic(
-        {
-            {"greeting", "Hello, world!\n"},
-            {"getInteger", "Please enter a number: "},
-            {"printResult", "The final result is: "}
-        });
-
+static unordered_map<string, string>
+    generic({{"greeting", "Hello, world!\n"},
+             {"getInteger", "Please enter a number: "},
+             {"printResult", "The final result is: "}});
 
 // function signatures
-BigBoi fib (const BigBoi);
-BigBoi fibo (const BigBoi, const BigBoi, const BigBoi);
-
+BigBoi fib(const BigBoi);
+BigBoi fibo(const BigBoi, const BigBoi, const BigBoi);
 
 int main() {
-    cout << generic["greeting"];
-    cout << generic["getInteger"];
+  cout << generic["greeting"];
+  cout << generic["getInteger"];
 
-    BigBoi input;
-    cin >> input;
+  BigBoi input;
+  cin >> input;
 
-    cout << generic["printResult"];
-    auto result = fib(input);
-    cout << result << endl;
+  cout << generic["printResult"];
+  auto result = fib(input);
+  cout << result << endl;
 
-    return 0;
+  return 0;
 }
-
 
 // wrapper function for the core fibonacci function
 BigBoi fib(const BigBoi n) {
-    return fibo(n, 0, 1);
-    // unoptimized recursive calls
-    //return  (n == 0) ? 0 :
-            //(n == 1) ? 1 : fib(n-1) + fib(n-2);
+  return fibo(n, 0, 1);
+  // unoptimized recursive calls
+  // return  (n == 0) ? 0 :
+  //(n == 1) ? 1 : fib(n-1) + fib(n-2);
 }
-
 
 // recursive function with optimization for tail calls
-BigBoi fibo (const BigBoi i, const BigBoi pre, const BigBoi cur) {
-    cout << i << "\t: " << pre << endl;
-    if (i == 0) return cur;
-    return fibo(i-1, cur, pre+cur);
+BigBoi fibo(const BigBoi i, const BigBoi pre, const BigBoi cur) {
+  cout << i << "\t: " << pre << endl;
+  if (i == 0)
+    return cur;
+  return fibo(i - 1, cur, pre + cur);
 }
-
